8af6dc268c12450665050bccc47f4324fb473b94
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2020-08-21 13:04:25 +0300

    FirstComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
