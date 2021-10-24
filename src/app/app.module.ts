31c5901bff08944ecdd96c7d977fd60315def9f2
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2021-10-25 00:43:27 +0300

    Setting_Up_Database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 
 import { from } from 'rxjs';
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,24 +11,29 @@ export class RecipeService{
      recipesChanged= new Subject<Recipe[]>();
 
   
-   private recipes:Recipe[]=[
-        new Recipe('Tasty Schnitzel',
-        'A super-tasty Schnitzel - just awesome',
-        'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
-        [
-          new Ingredient('Meat',1),
-          new Ingredient('French Fries',20)
-        ]),
-        new Recipe('Big Fat Burger',
-        'What else you need to say',
-        'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
-       [
-        new Ingredient('Buns',2),
-        new Ingredient('Meat',1)
-       ])
-      ];
-
+//    private recipes:Recipe[]=[
+//         new Recipe('Tasty Schnitzel',
+//         'A super-tasty Schnitzel - just awesome',
+//         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+//         [
+//           new Ingredient('Meat',1),
+//           new Ingredient('French Fries',20)
+//         ]),
+//         new Recipe('Big Fat Burger',
+//         'What else you need to say',
+//         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+//        [
+//         new Ingredient('Buns',2),
+//         new Ingredient('Meat',1)
+//        ])
+//       ];
+      private recipes: Recipe[]=[];
       constructor(private slService:ShoppingListService) {}     
+      setRecipes(recipes: Recipe[]){
+           this.recipes=recipes;
+           this.recipesChanged.next(this.recipes.slice());
+      }
+
 
       getRecipes(){
           return this.recipes.slice();
