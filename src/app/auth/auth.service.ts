c3c7224ede9d622035e2af9e80ca44ed2baa4f2a
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2021-11-12 16:55:58 +0200

    Auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -62,6 +62,29 @@ return this.http.post<AuthResponseData>(
         })
         );
        }
+   
+       autoLogin(){
+          const userData:{
+             email:string;
+             id:string;
+             _token:string;
+             _tokenExpirationDate:string;
+          }=JSON.parse(localStorage.getItem('userData')) ;
+          if(!userData){
+            return;
+          }
+          const loadedUser= new User(userData.email,
+            userData.id,
+            userData._token,
+            new Date( userData._tokenExpirationDate)
+           );
+           if(loadedUser.token){
+              this.user.next(loadedUser);
+           }
+       }
+
+
+
        logout(){
            this.user.next(null);
            this.router.navigate(['/auth']);
@@ -75,6 +98,7 @@ return this.http.post<AuthResponseData>(
            expirationDate
            );
         this.user.next(user);
+        localStorage.setItem('userData',JSON.stringify(user));
 
        }
 
