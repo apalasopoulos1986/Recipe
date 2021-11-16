05879c8e5f92985b15fda0b72cc39ba38b9b70bb
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2021-11-16 12:50:01 +0200

    Alert-Modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AlertComponent } from './shared/alert/alert.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthComponent } from './auth/auth.component';
@@ -42,7 +43,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -53,5 +53,8 @@ export class AuthComponent{
     form.reset();
     
     }
+    onHandleError(){
+        this.error=null;
+    }
     
 }
