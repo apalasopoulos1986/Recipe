c7319ff84ebad7497466da44b320ec89daad0d9a
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2021-11-17 12:10:00 +0200

    Splitting the app into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RecipesModule } from './recipes/recipes.module';
 import { AlertComponent } from './shared/alert/alert.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
@@ -15,33 +16,24 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
+
+
 import { AppComponent } from './app.component';
 
 import { from } from 'rxjs';
 import { RecipesComponent } from './recipes/recipes.component';
 import {HeaderComponent} from './header/header.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -51,7 +43,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [ShoppingListService, RecipeService, {
     provide:HTTP_INTERCEPTORS,
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,33 @@
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { AuthGuard } from './../auth/auth.guard';
+import { RecipesResolverService } from './recipes-resolver.service';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes.component';
+import{RecipeEditComponent} from './recipe-edit/recipe-edit.component'
+
+import{NgModule} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+const routes:Routes=[{
+    path:'recipes',
+    component:RecipesComponent,
+    canActivate:[AuthGuard],
+    children:[
+       {path: '', component:RecipeStartComponent},
+       {path:'new', component:RecipeEditComponent},
+       {path:':id',
+        component:RecipeDetailComponent, 
+        resolve:[RecipesResolverService]
+       },       
+       {path:':id/edit',
+       component:RecipeEditComponent,
+       resolve:[RecipesResolverService]
+       },
+     ]
+   }
+];
+@NgModule({
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+
+})
+export class RecipesRoutingModule{}
